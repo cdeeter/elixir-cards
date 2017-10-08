96f232caf15dc0fca6865d4019a851c12f287ab3
Celia Deeter <celia@peek.com>
2017-10-08 12:02:36 -0700

    Added documentation

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,5 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
 
+  @doc """
+    Returns a deck (list of strings) representing a deck of playing cards
+  """
   def create_deck do
     values = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"]
     suits = ["hearts", "diamonds", "spades", "clubs"]
@@ -9,23 +15,49 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Accepts a `deck` (list) and returns a shuffled version of the `deck`.
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Accepts a `deck` (list) and a `card` (string) and returns a boolean
+    based on whether or not that `deck` contains that `card`.
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a `deck` (list) into a `hand` (list) and returns the
+    remainder of the `deck`. The `hand_size` (integer) argument
+    indicates how many cards should be in the `hand`.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+    Accepts a `deck` (list) and a `filename` (string) and saves it.
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
+  @doc """
+    Accepts a `filename` (string) and returns the saved file.
+  """
   def load(filename) do
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term binary
@@ -33,8 +65,12 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Accepts a `hand_size` (integer) returns a shuffled `hand` (list)
+    of the size you passed in.
+  """
   def create_hand(hand_size) do
-    {hand, _deck} = Cards.shuffle(create_deck()) |> Cards.deal(hand_size)
+    {hand, _deck} = create_deck |> Cards.shuffle |> Cards.deal(hand_size)
 
     hand
   end
